05310b516cba6771f82a03dc217ce3795da16f5b
Astro <astrocket.dev@gmail.com>
2025-02-24 16:21:54 -0600

    add default color setting link icon

--- src/index.js
+++ src/index.js
@@ -172,6 +172,8 @@ export default class AnyButton {
       colorsHolder: "anyButtonContainer__colorsHolder",
       colorHolder: "anyButtonContainer__colorHolder",
       colorHolderLabel: "anyButtonContainer__colorHolder__label",
+      colorSettingHolder: "anyButtonContainer__colorSettingHolder",
+      colorSettingLink: "anyButtonContainer__colorSettingLink",
       inputText: "anyButtonContainer__input--text",
       inputLink: "anyButtonContainer__input--link",
       inputColor: "anyButtonContainer__input--color",
@@ -332,6 +334,19 @@ export default class AnyButton {
 
     colorsHolder.appendChild(textColorHolder);
     colorsHolder.appendChild(backgroundColorHolder);
+
+    if (this.config.colorSettingLink) {
+      const colorSettingHolder = this.make('div', [this.CSS.colorSettingHolder]);
+      const colorSettingLink = this.make('a', [this.CSS.colorSettingLink], {
+        href: this.config.colorSettingLink,
+        target: '_blank',
+        rel: 'nofollow noindex noreferrer',
+      });
+
+      colorSettingLink.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2"><path d="M11.992 21c-.728 -.003 -1.455 -.442 -1.667 -1.317a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c.882 .214 1.32 .95 1.317 1.684"></path><path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path><path d="M19 22.5a4.75 4.75 0 0 1 3.5 -3.5a4.75 4.75 0 0 1 -3.5 -3.5a4.75 4.75 0 0 1 -3.5 3.5a4.75 4.75 0 0 1 3.5 3.5"></path></svg>`;
+      colorSettingHolder.appendChild(colorSettingLink);
+      colorsHolder.appendChild(colorSettingHolder);
+    }
 
     inputHolder.appendChild(this.nodes.textInput);
     inputHolder.appendChild(this.nodes.linkInput);
